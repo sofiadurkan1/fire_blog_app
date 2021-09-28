80c0ba3bfc2844425d1275f1405d2bcff290c4a7
Sofia <sofiadurkan@gmail.com>
2021-09-28 11:06:11 -0500

    update and delete button visibilitty  adjusted

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,7 +2,6 @@
 // import { useParams } from 'react-router';
 // import { useFetch } from '../helpers/functions';
 
-
 // const Details = () => {
 //   const { blogsWithId, isLoading } = useFetch();
 
@@ -11,14 +10,14 @@
 //     return (
 //         <div>
 //             <h1>Details</h1>
-            
+
 //         </div>
 //     )
 // }
 
 // export default Details
 
-import React from "react";
+import React, { useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -28,92 +27,90 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import { useHistory, useParams } from 'react-router';
-import { deleteHandler, updateInfo } from '../helpers/functions';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import Grid from '@material-ui/core/Grid';
-
+import { useHistory, useParams } from "react-router";
+import { deleteHandler, updateInfo } from "../helpers/functions";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import Grid from "@material-ui/core/Grid";
+import { AuthContext } from "../contexts/AuthContext";
 
 const useStyles = makeStyles({
   root: {
     // maxWidth: 345,
 
-  
-    height:"100vh",
-    width:"100vh",
-    alignItem:"center"
+    height: "100vh",
+    width: "100vh",
+    alignItem: "center",
   },
-
-
-  
-
 });
 
 export default function ImgMediaCard() {
   const classes = useStyles();
-  const selectedTitle  = localStorage.getItem('title')
-  const selectedId  = localStorage.getItem('id')
-  const selectedImage  = localStorage.getItem('image')
-  const selectedContent  = localStorage.getItem('content')
-  const selectedUser  = localStorage.getItem('user')
+  const selectedTitle = localStorage.getItem("title");
+  const selectedId = localStorage.getItem("id");
+  const selectedImage = localStorage.getItem("image");
+  const selectedContent = localStorage.getItem("content");
+  const selectedUser = localStorage.getItem("user");
 
-let history = useHistory()
-  const deleteAndNavigate =(id)=>{
-    deleteHandler(id)
-    history.push("/")
-    
+  const { currentUser } = useContext(AuthContext);
 
-  }
-  const storeAndNavigate =()=>{
-    history.push("/newBlog")
+  const [showButton, setShowButton] = useState(currentUser?.email === selectedUser);
 
 
+  let history = useHistory();
+  const deleteAndNavigate = (id) => {
+    deleteHandler(id);
+    history.push("/");
+  };
+  const storeAndNavigate = () => {
+   
+    history.push("/newBlog");
 
-
-  }
+  };
   return (
-      <Grid container
+    <Grid
+      container
       spacing={0}
       direction="column"
       alignItems="center"
       justify="center"
-      style={{ minHeight: '100vh' }}>
-    <Card className={classes.root} >
-      <CardContent>
-      <CardMedia
-      
-            className={classes.media}
-          />
+      style={{ minHeight: "100vh" }}
+    >
+      <Card className={classes.root}>
+        <CardContent>
+          <CardMedia className={classes.media} />
           <img src={selectedImage} alt="" />
-        <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant="h5" component="h2">
             {selectedTitle}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {selectedContent}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {selectedUser}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="comment">
-            <ChatBubbleOutlineIcon/>
-        </IconButton>
-        
-      </CardActions>
-    
-
-
-    </Card>
-    <Button onClick={() =>storeAndNavigate()}>UPDATE</Button>
-    <Button textAlign="center"
-                  className="delete"
-                  onClick={() => deleteAndNavigate(selectedId)}>DELETE</Button>
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {selectedContent}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {selectedUser}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="comment">
+            <ChatBubbleOutlineIcon />
+          </IconButton>
+        </CardActions>   
+      </Card>
+         { showButton ? 
+         (
+           <>
+         <Button  onClick={() => storeAndNavigate()} > UPDATE</Button> 
+          <Button
+            textAlign="center"
+            className="delete"
+            onClick={() => deleteAndNavigate(selectedId)}
+          >
+            DELETE
+          </Button>
+          </> ): ""
+          }
     </Grid>
-    
   );
 }
-
